a8e8d3a68e8a7a2b58575fec2357109de4980fa6
adrian <raineadrian@gmail.com>
2019-01-09 22:39:21 +0200

    Initial commit

---
+++ run.py
@@ -0,0 +1,30 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+
+import os
+
+app = Flask(__name__)
+
+api = Api(app)
+
+@app.route("/")
+def start():
+    """Render README file"""
+
+class GuessGender(Resource):
+    def get(self):
+        response = 'Hi, let me guess your gender by giving me your name.'
+
+        return {'responseCode': '000', 'message': response}, 200
+
+    def post(self):
+        name = request.json['name']
+
+        response = name + '? Sounds female.'
+
+        return {'responseCode': '000', 'message': response}, 201
+
+api.add_resource(GuessGender, '/guessgender')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True)
